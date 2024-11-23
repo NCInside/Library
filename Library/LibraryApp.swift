9bf2707720cbbfc13a7ad24023d3255b5a9d753a
NCInside <nchristian02@ciputra.ac.id>
2024-11-23 09:48:22 +0700

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Nicholas Christian Irawan on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
